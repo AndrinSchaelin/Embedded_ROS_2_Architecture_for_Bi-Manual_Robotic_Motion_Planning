6476ea4abeab19803fc89e9113d61c4f8fb16139
Andrin Schalin <aschalin@mavt-ide-n105l.d.ethz.ch>
2025-03-29 10:55:56 +0100

    Workspace building

---
+++ build/environment/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/aschalin/UR5e_Assembly_of_Wooden_Pieces/install/table_description/share/table_description/package.sh"
+. "/home/aschalin/UR5e_Assembly_of_Wooden_Pieces/install/urdf_tutorial/share/urdf_tutorial/package.sh"
